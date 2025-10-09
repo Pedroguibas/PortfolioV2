31f7220ee474ff59a3c779f5f759d889ae06d114
Pedroguibas <pedroguibas123@gmail.com>
2025-10-09 19:49:25 -0300

    Feat: Implements Infinite Scroller
    Shows an autoscroller under the Hero Section to show my softskills

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Layout from "./components/shared/Layout";
 import Hero from "./components/Hero";
+import SoftskillsScroller from "./components/SoftskillsScroller";
 
 function App() {
   return (
     <>
       <Layout>
         <Hero />
+        <SoftskillsScroller />
       </Layout>
     </>
   );
---
+++ src/components/SoftskillsScroller.tsx
@@ -0,0 +1,57 @@
+import { useLanguage } from "./shared/LanguageContext";
+import { useState, useEffect, useRef } from "react";
+import "../assets/css/softskillsScroller.css";
+
+export function SoftskillsListItems() {
+  const { txt } = useLanguage();
+  const items = txt.softskills;
+
+  return (
+    <>
+      {items.map((item, i) => (
+        <li className="softskillsItem relative" key={i}>
+          {item}
+        </li>
+      ))}
+    </>
+  );
+}
+
+export function SoftsKillsList() {
+  const [itemsRepeatCount, setItemsRepeatCount] = useState(0);
+  const ref = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    const total_w = 2 * window.innerWidth;
+
+    if (ref.current) {
+      const comp_w = ref.current.offsetWidth;
+
+      if (comp_w > 0) setItemsRepeatCount(Math.ceil(total_w / comp_w));
+    }
+  }, []);
+
+  return (
+    <>
+      <ul ref={ref} className="invisible h-0 pointer-none flex text-2xl w-max">
+        <SoftskillsListItems />
+      </ul>
+
+      <ul className="softskillsList flex text-2xl pt-1 pb-0.5 w-max">
+        {Array.from({ length: Math.max(itemsRepeatCount, 0) }).map(
+          (u_val, i) => (
+            <SoftskillsListItems key={i} />
+          )
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default function SoftskillsScroller() {
+  return (
+    <section className="softskillsScroller bg-[var(--highlight-primary)] text-white w-full overflow-x-hidden">
+      <SoftsKillsList />
+    </section>
+  );
+}
