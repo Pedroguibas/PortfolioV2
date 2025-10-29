a8c41570ea66955a5cf04162d9c42d3bf86b4433
Pedroguibas <pedroguibas123@gmail.com>
2025-10-29 11:47:36 -0300

    feat: implements dynamic image import

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BoxArrowUpRight } from "react-bootstrap-icons";
 import { useLanguage } from "./shared/LanguageContext.tsx";
 import projects from "../assets/data/projects.json";
-import testImg from "../assets/projects/portfolio.png";
 import "../assets/css/projects.css";
 
 interface ProjectsProps {
@@ -15,8 +14,14 @@ interface ProjectProps {
   p_txt: ProjectJSON;
 }
 
+const images = import.meta.glob<{ default: string }>(
+  "../assets/projects/*.png",
+  { eager: true }
+);
+
 function Project({ p_txt }: ProjectProps) {
   const { language } = useLanguage();
+  const image = images[`../assets/projects/${p_txt.image}.png`]?.default;
   return (
     <button className="projectCard flex flex-col group text-start relative border-2 rounded-2xl transition-all duration-500 ease-in-out cursor-pointer">
       <span className="redirectIcon flex justify-center items-center absolute top-2 right-2 p-2 rounded-full bg-[var(--highlight-secondary)] text-white w-fit z-2">
@@ -26,7 +31,7 @@ function Project({ p_txt }: ProjectProps) {
         <aside className="overflow-hidden rounded-t-2xl h-48">
           <img
             className="object-cover w-full h-full group-hover:scale-110 transition-transform duration-700 ease-in-out"
-            src={testImg}
+            src={image}
             alt=""
           />
         </aside>
