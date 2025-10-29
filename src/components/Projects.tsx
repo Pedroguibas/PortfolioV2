0549b9746962ae56702d667937a43422d3aebe6e
Pedroguibas <pedroguibas123@gmail.com>
2025-10-29 13:59:17 -0300

    feat: implements links to github and projects actual data

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -22,8 +22,16 @@ const images = import.meta.glob<{ default: string }>(
 function Project({ p_txt }: ProjectProps) {
   const { language } = useLanguage();
   const image = images[`../assets/projects/${p_txt.image}.png`]?.default;
+
+  const handleClick = () => {
+    window.location.href = p_txt.source;
+  };
+
   return (
-    <button className="projectCard flex flex-col group text-start relative border-2 rounded-2xl transition-all duration-500 ease-in-out cursor-pointer">
+    <button
+      onClick={handleClick}
+      className="projectCard flex flex-col group text-start relative border-2 rounded-2xl transition-all duration-500 ease-in-out cursor-pointer"
+    >
       <span className="redirectIcon flex justify-center items-center absolute top-2 right-2 p-2 rounded-full bg-[var(--highlight-secondary)] text-white w-fit z-2">
         <BoxArrowUpRight />
       </span>
@@ -35,7 +43,7 @@ function Project({ p_txt }: ProjectProps) {
             alt=""
           />
         </aside>
-        <div className="flex flex-col justify-start py-2 px-4">
+        <div className="flex flex-col text-sm md:text-base justify-start py-2 px-4">
           <h2 className="text-2xl mt-1">
             <b>{p_txt.projectTexts[language].projectTitle}</b>
           </h2>
